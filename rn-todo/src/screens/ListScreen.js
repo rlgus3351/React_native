5181d5bd6a1572a19b447694722ff9bc0ec229a3
권기현 <rlgus3351@naver.com>
2023-12-24 15:25:25 +0900

    23/12/24 ListScree Editing

--- rn-todo/src/screens/ListScreen.js
+++ rn-todo/src/screens/ListScreen.js
@@ -1,10 +1,14 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-const ListScreen = () => {
+const ListScreen = ({navigation}) => {
 
     return(
         <View style={Styles.container}>
             <Text style={{fontSize:30}}>List Screen</Text>
+            <Button title='push' onPress={() => navigation.push('list')}/>
+            <Button title="navigate"
+                onPress={()=> navigation.navigate('list')}
+            />
         </View>
     );
 };
