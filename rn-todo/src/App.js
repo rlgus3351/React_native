110c9c80b7602b77f4e129ee3e19c4a4e8d9a0cd
권기현 <rlgus3351@naver.com>
2024-01-02 17:58:46 +0900

    24/01/02 Use Context API

--- rn-todo/src/App.js
+++ rn-todo/src/App.js
@@ -3,22 +3,30 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './navigations/AuthStack';
 import { useState } from 'react';
 import MainStack from './navigations/MainStack';
+import UserContext from './contexts/UserContext';
 
 
 const App = () =>{
   const [user, setUser] = useState(null);
   return (
-    <NavigationContainer>
-      
-      <StatusBar style="dark" />
-      {user ? (
-        <MainStack user = {user} setUser = {setUser}/>
-      ):(
-        <AuthStack user = {user} setUser = {setUser}/>
-      )}
-    </NavigationContainer>
-  );
+    <UserContext.Provider value={'gihyun'}>
+      <NavigationContainer>
+
+        <StatusBar style="dark" />
+        {user ? (
+          <MainStack user = {user} setUser = {setUser}/>
+        ):(
+          <AuthStack user = {user} setUser = {setUser}/>
+        )}
+      </NavigationContainer>
+    </UserContext.Provider>
+    );
+    
 }
+
+
+
+
 
 export default App;
 
--- rn-todo/src/navigations/AuthStack.js
+++ rn-todo/src/navigations/AuthStack.js
@@ -1,10 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SingInScreen from '../screens/SingInScreen';
-import ListScreen from '../screens/ListScreen';
-import { PRIMARY,WHITE } from '../colors';
-import HeaderLeftButton from '../components/HeaderLeftButton';
-import HeaderRightButton from '../components/HeaderRightButton';
-import SettingsScreen from '../screens/SettingScreen';
+import { WHITE } from '../colors';
+
 
 const Stack = createNativeStackNavigator();
 
