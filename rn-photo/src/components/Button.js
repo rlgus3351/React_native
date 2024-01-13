7c966b3829da19799f3ba6814c049c9f9726b701
권기현 <rlgus3351@naver.com>
2024-01-13 14:18:42 +0900

    24/01/13 Create Custom Button

--- rn-photo/src/components/Button.js
+++ rn-photo/src/components/Button.js
@@ -1,20 +1,64 @@
 import { ActivityIndicator, Pressable, StyleSheet, Text, View } from "react-native";
-import propType from 'prop-types';
+import propTypes from 'prop-types';
 import {WHITE, GRAY,PRIMARY} from '../colors';
 
-const Button = ({}) => {
+const Button = ({sytles, title, onPress, disabled, isLoading}) => {
     return(
-
+        <View style={[defaultStyles.container, sytles?.container]}>
+            <Pressable
+                onPress={()=> onPress()}
+                disabled={disabled|| isLoading}
+                style={({ pressed}) =>[
+                    defaultStyles.button,
+                    {
+                        backgroundColor : (() => {
+                            switch (true){
+                                case disabled || isLoading:
+                                    return PRIMARY.LIGTH;
+                                case pressed:
+                                    return PRIMARY.DARK;
+                                default :
+                                    return PRIMARY.DEFAULT;
+                            }
+                        })(),
+                    },
+                    sytles?.button,
+                ]}
+            >
+                {isLoading ? (
+                    <ActivityIndicator size={"small"} color={GRAY.DARK} />
+                ) : (
+                    <Text style ={[defaultStyles.title, styles?.title]}>{title}</Text>
+                )}
+            </Pressable>
+        </View>
     );
 };
 
 
 Button.propTypes = {
-
+    styles:propTypes.object,
+    title:propTypes.string.isRequired,
+    onPress:propTypes.func.isRequired,
+    disabled:propTypes.bool,
+    isLoading:propTypes.bool,
 };
 
 const defaultStyles = StyleSheet.create({
-
+    container:{
+        width:'100%',
+    },
+    button:{
+        paddingVertical:20,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    title:{
+        color:WHITE,
+        fontSize:16,
+        fontWeight:'700',
+        lineHeight:20,
+    },
 });
 
 export default Button;
