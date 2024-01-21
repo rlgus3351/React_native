085a10a8c02503a30b6a6a803e22da29c2891a40
권기현 <rlgus3351@naver.com>
2024-01-21 23:53:33 +0900

    24/01/21 expo-splash-screen install

--- rn-photo/src/navigations/Navigation.js
+++ rn-photo/src/navigations/Navigation.js
@@ -1,7 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AuthStack from "./AuthStack";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+
 
 const Navigation =  () => {
+    useEffect(()=>{
+        (async () =>{
+            try{
+                await SplashScreen.preventAutoHideAsync();
+            }catch(e){
+                console.log(e);
+            }
+        })();
+    },[]);
     return (
         <NavigationContainer>
             <AuthStack/>
